6b60ee6dd7bb6b145a748933ab70929b3371321b
HurmuzacheCiprian <hurmuzache.ciprian@gmail.com>
2015-03-23 11:49:27 +0200

    persistence xml

--- Licenta/licenta-persistence/src/main/java/licenta/App.java
+++ Licenta/licenta-persistence/src/main/java/licenta/App.java
@@ -1,5 +1,14 @@
 package licenta;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import licenta.persistence.dao.impl.CustomerDaoImpl;
+import licenta.persistence.entities.CustomerEntity;
+
 /**
  * Hello world!
  *
@@ -9,5 +18,26 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+       /* EntityManagerFactory emfactory =  HibernatePersistenceProvider.createEntityManagerFactory( "persistence.xml" );
+        
+        EntityManager entitymanager = emfactory.createEntityManager( );
+        entitymanager.getTransaction( ).begin( );
+
+        CustomerDaoImpl customerDao = new CustomerDaoImpl();
+        customerDao.setEntityManager(entitymanager);
+        
+        CustomerEntity customer = new CustomerEntity();
+        customer.setName("bibi");
+        
+        customerDao.persist(customer);
+        entitymanager.persist(customer);
+
+        entitymanager.getTransaction( ).commit( );
+
+        entitymanager.close( );
+        emfactory.close( );
+        */
+        
     }
 }
